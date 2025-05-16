f20c2cfa93f5c206e048deecaacb1ff9a73dd5d0
Dozier Tanika Danielle <dozierdanielle@ymail.com>
2025-05-16 02:47:27 -0700

    Complete inventory management functions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,39 @@
 // Write your code here
+//Create Product Inventory Array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+  console.log(products);
+  //Output: "Laptop", "Phone", "Headphones", "Monitor"];
 
+//Access Product Information
+function logFirstProduct() {
+  return products[0];
+}
+console.log(logFirstProduct());
+//Output: "Laptop" 
 
+//Add Product
+function addProduct(productName) {
+  products.push(productName);
+}
+addProduct("Case");
+console.log(products);
+//Output: "Laptop", "Phone", "Headphones", "Monitor", "Case"
+
+//Update Product Information
+function updateProductName(productName) {
+  products.push(productName);
+}
+products[1] = "Pager";
+console.log(products); 
+//Output: "Laptop", "Pager", "Headphones", "Monitor", "Case";
+
+//Remove Product
+function removeLastProduct(productName) {
+  products.pop();
+}
+removeLastProduct();
+console.log(products);
+//Output: "Laptop, "Pager", "Headphones", "Monitor";
 
 // Export the necessary parts for testing
 module.exports = {
